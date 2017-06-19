0a63ebb9360b11ad71a0de9a5c16b0703c0f9b63
Luc <luc.tribolet@gmail.com>
2017-06-19 22:00:26 +0200

    Start playing around with queries

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Overgang WP theme
 
 Based on [_s](http://underscores.me)
+
+Requires :
+
+- [AdvancedCustomFields](https://www.advancedcustomfields.com)
--- index.php
+++ index.php
@@ -12,45 +12,52 @@
  * @package overgang
  */
 
-get_header(); ?>
+//get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
 			<?php
-			endif;
+			if ( have_posts() ) :
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                /*
+                 * GET MAGAZINE
+                 */
+				$magazine = get_terms( [ 'taxonomy' => 'magazine' ], [
+					'orderby' => 'none',
+					'order'   => 'DESC',
+					'number'  => 1
+				] );
+				$latest_magazine = reset( $magazine );
 
 				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 * GET POSTS BY CATEGORIES
 				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+				$args = [
+					'tax_query'     => [
+						[
+							$latest_magazine->slug
+						]
+					],
+					'category_name' => 'interview'
+				];
 
-			endwhile;
+				$wp_query = new WP_Query( $args );
+				/* Start the Loop */
+				while ( $wp_query->have_posts() ) : $wp_query->the_post();
+					the_title();
+				endwhile;
 
-			the_posts_navigation();
 
-		else :
+			else :
 
-			get_template_part( 'template-parts/content', 'none' );
+				//get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+			endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
-get_sidebar();
-get_footer();
+//get_sidebar();
+//get_footer();
