c21d8b495fb0771e67f278d2538eb5cb036a4979
Luc <luc.tribolet@gmail.com>
2017-06-24 15:01:54 +0200

    Add bootstrap and setup grid for home

--- functions.php
+++ functions.php
@@ -67,21 +67,11 @@ function overgang_setup() {
 
 	// Add theme support for selective refresh for widgets.
 	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	show_admin_bar(false);
 }
 endif;
 add_action( 'after_setup_theme', 'overgang_setup' );
-
-/**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
- */
-function overgang_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'overgang_content_width', 640 );
-}
-add_action( 'after_setup_theme', 'overgang_content_width', 0 );
 
 /**
  * Register widget area.
